5154656e6bad524a01b1256bc195d13df3003abc
Thiago Almeida <tcalmeida85@gmail.com>
2022-12-10 00:44:13 -0300

    cria o servidor e a conexão com o banco de dados

---
+++ src/database/index.js
@@ -0,0 +1,33 @@
+const sequelize = require ("sequelize");
+
+const DB_NAME = "";     // inserir nome do banco de dados
+const DB_USER = "root";
+const DB_PASS ="";      // inserir senha
+const DB_CONFIG =  {
+    dialect: "mysql",
+    host: "localhost",
+    port: 3306,
+};
+
+let db = {};
+
+try {
+    db = new sequelize(DB_NAME, DB_USER, DB_PASS, DB_CONFIG);
+  } catch (error) {
+    console.error("Não foi realizar a conexão");
+  }
+  
+  async function connection() {
+    try {
+      await db.authenticate();
+      console.log("Banco de dados conectado!");
+    } catch (error) {
+      console.log("Erro ao tentar se conectar com o banco de dados");
+    }
+  };
+  
+  Object.assign(db, {
+    connection,
+  });
+  
+  module.exports = db;
