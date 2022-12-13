2e475faadb1396f74f44140a46b1d427e025213c
Thiago Almeida <tcalmeida85@gmail.com>
2022-12-13 18:23:00 -0300

    cria a autenticação da entidade psicologos

--- src/middlewares/handleError.js
+++ src/middlewares/handleError.js
@@ -3,12 +3,10 @@ const { ValidationError } = require("express-validation");
 module.exports = (error, req, res, next) => {
   try {
     if (error instanceof ValidationError) {
-        return res.status(error.statusCode).json(error);
-      }
-
-
-
-  } catch(error) {
+      return res.status(error.statusCode).json(error);
+    }
+    
+  } catch (error) {
     return res.status(500).json(error);
   }
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const psicologosController = require("../controller/psicologosController");
 const psicologosValidation = require("../validations/psicologos/create");
-
-
+const authController = require("../controller/authController");
+const authLoginValidation = require("../validations/auth/login");
 
 
 
@@ -13,6 +13,8 @@ routes.get("/psicologos/:id", psicologosController.buscarPsiId);
 routes.post("/psicologos", psicologosValidation, psicologosController.cadastrarPsi);
 routes.put("/psicologos/:id", psicologosValidation, psicologosController.atualizarPsi);
 routes.delete("/psicologos/:id", psicologosController.deletarPsi);
+
+routes.post("/login", authLoginValidation, authController.login);
 
 // Opcional
 // routes.get("/dashboard/numero-pacientes", dashController.pacientes);
