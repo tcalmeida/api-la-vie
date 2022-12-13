3e5f38d20b6b99c8ef4356b0a9b70fb8a0b16e56
Christhofer94 <christhofer94@gmail.com>
2022-12-12 22:36:15 -0300

    cracao rotas atendimentos

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,6 +9,13 @@ routes.post("/psicologos", psicologosController.cadastrarPsi);
 routes.put("/psicologos/:id", psicologosController.atualizarPsi);
 routes.delete("/psicologos/:id", psicologosController.deletarPsi);
 
+
+//Criando rotas atendimentos
+routes.get("/atendimentos", );
+routes.get("/atendimentos/:id", )
+routes.post("/atendimentos", )
+
+
 // Opcional
 // routes.get("/dashboard/numero-pacientes", dashController.pacientes);
 // routes.get("/dashboard/numero-atendimentos", dashController.atendimentos);
