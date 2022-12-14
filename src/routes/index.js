9ce0983e00efd348df732dfa1ed8ea0b8a43b587
Thiago Almeida <tcalmeida85@gmail.com>
2022-12-13 23:31:51 -0300

    cria os endpoints e conta o número de psicologos

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const psicologosController = require("../controller/psicologosController");
 const psicologosValidation = require("../validations/psicologos/create");
 const authController = require("../controller/authController");
 const authLoginValidation = require("../validations/auth/login");
-
+const dashController = require("../controller/dashController");
 
 
 const routes = express.Router();
@@ -19,7 +19,7 @@ routes.post("/login", authLoginValidation, authController.login);
 // Opcional
 // routes.get("/dashboard/numero-pacientes", dashController.pacientes);
 // routes.get("/dashboard/numero-atendimentos", dashController.atendimentos);
-// routes.get("/dashboard/numero-psicologos", dashController.psicologos);
+routes.get("/dashboard/numero-psicologos", dashController.psicologos);
 // routes.get("/dashboard/media-atendimentos", dashController.medAtendimentos);
 
 module.exports = routes;
